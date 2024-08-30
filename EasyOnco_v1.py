c12a26ea792216e6507e33692058350a20f3c5d0
LTK <xorrb85@gmail.com>
2024-08-30 17:18:23 +0900

    asd

---
+++ EasyOnco_v1.py
@@ -0,0 +1,150 @@
+import os
+import pandas as pd
+import argparse
+import sys
+import re
+import warnings
+#----------------------------------------------------------------------------------------#
+def transform_args(value) :
+    mapping = {
+        'P' : 'Pathogenic.VUS',
+        'A' : 'All.Variants'
+    }
+    if value in mapping :
+        return mapping[value]
+    else : 
+        raise argparse.ArgumentTypeError(f"Invalid choice: {value} (choose from {list(mapping.keys())})")
+#----------------------------------------------------------------------------------------#
+def mkMAF(file, args, path) :
+    global MAF
+    for sheet in args.sheets : 
+        tmp = pd.read_excel(f'{path}/{file}',sheet_name=sheet, engine='openpyxl')
+        tmp = tmp[tmp['select'].isin(args.filter)]
+        tmp.rename(columns={'gene':'Hugo_Symbol', 'ref':'Reference_Allele', 'alt':'Tumor_Seq_Allele2', 'VAF.var.freq':'i_TumorVAF_WU', 'NM':'i_transcript_name'}, inplace = True)
+        tmp['Chromosome'] = tmp['chrom.pos'].apply(lambda x:x.split(':')[0].replace('chr',""))
+        tmp['Start_Position'] = tmp['chrom.pos'].apply(lambda x:x.split(':')[1].split('-')[0])
+        tmp['Start_Position'] = pd.to_numeric(tmp['Start_Position'])
+        tmp['End_Position'] = tmp['chrom.pos'].apply(lambda x:x.split(':')[1].split('-')[1])
+        tmp['End_Position'] = pd.to_numeric(tmp['End_Position'])
+        tmp['Tumor_Sample_Barcode'] = file.split('.results.xlsx')[0]
+        tmp['Variant_Classification'] = ""
+        tmp['Variant_Type'] = ""
+        for i in range(len(tmp['HGVSc'])) :
+            if pd.isna(tmp['HGVSc'].iloc[i]):
+                pass
+            
+            elif '>' in tmp['HGVSc'].iloc[i] :
+                tmp.loc[tmp.index[i], 'Variant_Type'] = 'SNP'
+                if pd.isna(tmp['HGVSp'].iloc[i]) :
+                    if '+' in tmp['HGVSc'].iloc[i] or '-' in tmp['HGVSc'].iloc[i] :
+                        intron = r'[+-](\d+)'
+                        match = re.findall(intron, tmp['HGVSc'].iloc[i])
+                        if match :
+                            l = int(match[0])
+                            if l == 1 or l == 2 :
+                                tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Splice_Site'
+                            else :
+                                tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Intron_variant'
+                        else :
+                            continue
+                        
+                elif '=' in tmp['HGVSp'].iloc[i] :
+                    tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Silent_Mutation'
+                elif 'Ter' in tmp['HGVSp'].iloc[i] :
+                    tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Nonsense_Mutation'
+                else :
+                    tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Missense_Mutation'
+
+            elif 'delins' in tmp['HGVSc'].iloc[i] :
+                tmp.loc[tmp.index[i], 'Variant_Type'] = 'DELINS'
+                if pd.isna(tmp['HGVSp'].iloc[i]) :
+                    if '+' in tmp['HGVSc'].iloc[i] or '-' in tmp['HGVSc'].iloc[i] :
+                        intron = r'[+-](\d+)'
+                        match = re.findall(intron, tmp['HGVSc'].iloc[i])
+                        if match :
+                            l = int(match[0])
+                            if l == 1 or l == 2 :
+                                tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Splice_Site'
+                            else :
+                                tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Intron_variant'
+                        else :
+                            continue
+                else :
+                    if 'fs' in tmp['HGVSp'].iloc[i] :
+                        tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Frame_Shift_Indel'
+                    else :
+                        tmp.loc[tmp.index[i], 'Variant_Classification'] = 'In_Frame_Indel'
+
+            elif 'del' in tmp['HGVSc'].iloc[i] :
+                tmp.loc[tmp.index[i], 'Variant_Type'] = 'DEL'
+                if pd.isna(tmp['HGVSp'].iloc[i]) :
+                    if '+' in tmp['HGVSc'].iloc[i] or '-' in tmp['HGVSc'].iloc[i] :
+                        intron = r'[+-](\d+)'
+                        match = re.findall(intron, tmp['HGVSc'].iloc[i])
+                        if match :
+                            l = int(match[0])
+                            if l == 1 or l == 2 :
+                                tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Splice_Site'
+                            else :
+                                tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Intron_variant'
+                        else :
+                            continue
+                else :
+                    if 'fs' in tmp['HGVSp'].iloc[i] :
+                        tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Frame_Shift_Del'
+                    else :
+                        tmp.loc[tmp.index[i], 'Variant_Classification'] = 'In_Frame_Del'
+
+            elif 'ins' in tmp['HGVSc'].iloc[i] :
+                tmp.loc[tmp.index[i], 'Variant_Type'] = 'INS'
+                if pd.isna(tmp['HGVSp'].iloc[i]) :
+                    if '+' in tmp['HGVSc'].iloc[i] or '-' in tmp['HGVSc'].iloc[i] :
+                        intron = r'[+-](\d+)'
+                        match = re.findall(intron, tmp['HGVSc'].iloc[i])
+                        if match :
+                            l = int(match[0])
+                            if l == 1 or l == 2 :
+                                tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Splice_Site'
+                            else :
+                                tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Intron_variant'
+                        else :
+                            continue
+                else :
+                    if 'fs' in tmp['HGVSp'].iloc[i] :
+                        tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Frame_Shift_Ins'
+                    else :
+                        tmp.loc[tmp.index[i], 'Variant_Classification'] = 'In_Frame_Ins'
+            
+            elif 'dup' in tmp['HGVSc'].iloc[i] :
+                # Chromosomal -> Structure_variant로 정의해야하는지 -> 그렇다면 deletion 길이 보고 특정 길이를 넘어서면 그것도 Structure_variant로 따로 분류해야하는 건 아닌지 + 교수님 파이프라인이 SNV랑 INDEL만 볼 수 있는거면 inversion뿐만 아니라 dup도 관찰할 수 없어야하는 거 아닌지..? dup 결과가 나왔다면 확인은 하지 못했지만 inv도 존재할 가능성..?
+                tmp.loc[tmp.index[i], 'Variant_Type'] = 'INS' 
+                tmp.loc[tmp.index[i], 'Variant_Classification'] = 'Duplication'
+                
+        tmp['Tumor_Sample_Barcode'] = file.split('.')[0]
+        tmp.rename(columns={'HGVSp':'Protein_Change'}, inplace = True)
+        tmp['i_TumorVAF_WU'] = pd.to_numeric(tmp['i_TumorVAF_WU'])
+        tmp = tmp.assign(i_TumorVAF_WU=(tmp['i_TumorVAF_WU'] * 100).round(4))
+        tmp = tmp[['Hugo_Symbol','Chromosome','Start_Position','End_Position','Reference_Allele','Tumor_Seq_Allele2','Variant_Classification','Variant_Type','Tumor_Sample_Barcode','Protein_Change','i_TumorVAF_WU','i_transcript_name']]
+        MAF = pd.concat([MAF, tmp], axis=0, ignore_index=True)
+    # MAF.to_excel(f'{path}/OnGo.xlsx',index = False)
+    MAF.to_csv(f'{path}/EasyOnco.maf', index = False, sep ='\t')
+#----------------------------------------------------------------------------------------#
+def run() : 
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    parser = argparse.ArgumentParser(description='OnGoPlotter Usage')
+    parser.add_argument("-i", "--input", dest = "input", action = "store", nargs='+')
+    parser.add_argument("-f", "--selected_filter", dest = "filter", action = "store", nargs='+', type = str, default=['v'])
+    parser.add_argument("-s","--sheet", dest = "sheets", action = "store", nargs='+', choices=['P','A'], required = True, help='Pathogenic.VUS(P),All.Variants(A)')
+    args = parser.parse_args()
+    path = os.getcwd()
+    args.sheets = list(map(transform_args, args.sheets))
+    global MAF
+    MAF = pd.DataFrame(columns=['Hugo_Symbol','Chromosome','Start_Position','End_Position','Reference_Allele','Tumor_Seq_Allele2','Variant_Classification','Variant_Type','Tumor_Sample_Barcode','Protein_Change','i_TumorVAF_WU','i_transcript_name'])
+    for File in args.input :
+        mkMAF(File, args, path)
+
+    command = 'Rscript /labmed/00.Code/EasyOnco/Oncoplotter_v1.R'
+    os.system(command)
+#----------------------------------------------------------------------------------------#
+if __name__ == '__main__' : 
+    run()
---
+++ Oncoplotter_v1.R
@@ -0,0 +1,364 @@
+library(maftools)
+library(readxl)
+library(dplyr)
+library(scales)
+library(circlize)
+#------------------------------------------------------------------------------#
+if (file.exists('Clinical_annotation.txt')){
+  #------------------------------------------------------------------------------#
+  maf <- read.table('EasyOnco.maf', sep='\t', header=T)
+  Clinical_annotation <- read.table('Clinical_annotation.txt', sep='\t', header=T)
+  current_date <- format(Sys.Date(), "%y%m%d")
+  #------------------------------------------------------------------------------#
+  VariantClassification = c("Frame_Shift_Del",
+                            "Frame_Shift_Ins",
+                            "Splice_Site",
+                            "Translation_Start_Site",
+                            "Nonsense_Mutation",
+                            "Nonstop_Mutation",
+                            "In_Frame_Del",
+                            "In_Frame_Ins",
+                            "Missense_Mutation",
+                            "Start_Codon_Del",
+                            "Stop_Codon_Del",
+                            "Duplication",
+                            "Intron_variant")
+  #------------------------------------------------------------------------------#
+  Onco <-  read.maf(maf = maf, 
+                    verbose=T,
+                    vc_nonSyn = VariantClassification,
+                    clinicalData = Clinical_annotation)
+  #------------------------------------------------------------------------------#
+  write.mafSummary(maf = Onco, basename = 'Onco')
+  #------------------------------------------------------------------------------#
+  options_df <- read_excel("/labmed/00.Code/EasyOnco/oncoplot_options_v1.xlsx", col_names = TRUE)
+  options_list <- setNames(as.list(options_df$Value), options_df$Options)
+  get_option <- function(option_name) {
+    return(options_list[[option_name]])
+  }
+  #------------------------------------------------------------------------------#
+  Oncoprint_color <- c(
+    Frame_Shift_Del = get_option("Frame_Shift_Del"),
+    Frame_Shift_Ins = get_option("Frame_Shift_Ins"),
+    Splice_Site = get_option("Splice_Site"),
+    Nonsense_Mutation = get_option("Nonsense_Mutation"),
+    In_Frame_Del = get_option("In_Frame_Del"),
+    In_Frame_Ins = get_option("In_Frame_Ins"),
+    Missense_Mutation = get_option("Missense_Mutation"),
+    Multi_Hit = get_option("Multi_Hit"),
+    Translation_Start_Site = get_option("Translation_Start_Site"),
+    Nonstop_Mutation = get_option("Nonstop_Mutation"),
+    Start_Codon_Del = get_option("Start_Codon_Del"),
+    Stop_Codon_Del = get_option("Stop_Codon_Del"),
+    Duplication = get_option("Duplication"),
+    Intron_variant = get_option("Intron_variant"))
+  Oncoprint_color <- Oncoprint_color[!is.na(Oncoprint_color)]
+  #------------------------------------------------------------------------------#
+  top_gene <- if (get_option("Top gene") == "All") NULL else as.numeric(get_option("Top gene"))
+  if (is.null(top_gene)) {
+    top_genes <- getGeneSummary(Onco)$Hugo_Symbol  
+  } else {
+    top_genes <- getGeneSummary(Onco)$Hugo_Symbol[1:top_gene]
+  }
+  GeneCut <- length(top_genes)
+  #------------------------------------------------------------------------------#
+  plot_title <- get_option("Plot title")
+  #------------------------------------------------------------------------------#
+  vaf_plot <- if (get_option("VAF plot") == "yes") "yes" else FALSE
+  if(vaf_plot == "yes") {
+    Genes <- subsetMaf(maf = Onco, 
+                       genes = top_genes,
+                       fields = "i_TumorVAF_WU", 
+                       mafObj = FALSE)[,mean(i_TumorVAF_WU, na.rm = TRUE), Hugo_Symbol]
+    colnames(Genes)[2] = "VAF (%)"
+  } else {
+    Genes <- NULL
+  }
+  #------------------------------------------------------------------------------#
+  sample_order_option <- get_option("sample_cluster_by")
+  
+  if (!is.null(sample_order_option) && sample_order_option %in% colnames(Clinical_annotation)) {
+    sample_order <- Clinical_annotation %>%
+      arrange(.[[sample_order_option]]) %>%
+      pull(Tumor_Sample_Barcode)
+  } else {
+    sample_order <- NULL
+  }
+  #------------------------------------------------------------------------------#
+  annotation_colors <- options_df %>%
+    dplyr::filter(grepl("Annotation color", Options)) %>%
+    pull(Value)
+  clinical_features <- colnames(Clinical_annotation)[-1]
+  categorical_features <- options_df$Value[25:29]
+  
+  continuous_features <- options_df$Value[30:34]
+  continuous_features <- continuous_features[!continuous_features %in% c("NA", NA)]
+  
+  remove_na <- function(vec) {
+    vec[vec != "NA"]
+  }
+  categorical_colors <- options_df$Value[35:84]
+  categorical_groups <- split(categorical_colors, ceiling(seq_along(categorical_colors) / 10))
+  categorical_groups <- lapply(categorical_groups, remove_na)
+  
+  valid_categorical_features <- categorical_features[!categorical_features %in% c("NA", NA)]
+  
+  continuous_colors <- options_df$Value[85:94]
+  continuous_groups <- split(continuous_colors, ceiling(seq_along(continuous_colors) / 2))
+  continuous_groups <- lapply(continuous_groups, remove_na)
+  
+  valid_colors <- continuous_colors[continuous_colors != "NA"]
+  
+  Annotation_color <- list()
+  color_index <- 1
+  
+  for (feature in continuous_features) {
+    Onco@clinical.data[[feature]] <- as.numeric(Onco@clinical.data[[feature]])
+    valid_feature_values <- Clinical_annotation[[feature]]
+    
+    if (!is.null(valid_feature_values) && length(na.omit(valid_feature_values)) > 0) {
+      color_pair <- continuous_colors[(color_index):(color_index + 1)]
+      num_steps <- max(valid_feature_values, na.rm = TRUE) - min(valid_feature_values, na.rm = TRUE) + 1
+      feature_gradient <- colorRampPalette(color_pair)(num_steps)
+      
+      feature_breaks <- seq(min(valid_feature_values, na.rm = TRUE), 
+                            max(valid_feature_values, na.rm = TRUE), 
+                            length.out = num_steps)
+      
+      Annotation_color[[feature]] <- setNames(feature_gradient, as.character(round(feature_breaks)))
+      color_index <- color_index + 2
+    }
+  }
+  
+  for (i in seq_along(valid_categorical_features)) {
+    feature <- valid_categorical_features[i]
+    group_colors <- categorical_groups[[as.character(i)]]
+    unique_values <- unique(Clinical_annotation[[feature]])
+    
+    if (length(group_colors) >= length(unique_values)) {
+      Annotation_color[[feature]] <- setNames(group_colors[1:length(unique_values)], unique_values)
+    } 
+  }
+  #------------------------------------------------------------------------------#
+  if (tolower(options_list$`Top gene`) == 'all') {
+    Oncogenes <- Onco@gene.summary$Hugo_Symbol
+  } else {
+    top_gene_num <- as.numeric(Option$Top_gene)
+    Oncogenes <- Onco@gene.summary$Hugo_Symbol[1:top_gene_num]
+  }
+  GeneCut <- length(Oncogenes)
+  #------------------------------------------------------------------------------#
+  Onco.titv = titv(maf = Onco, plot = FALSE, useSyn = TRUE)
+  oncotitiv_filename <- paste0(current_date, "_onco.titv.pdf")
+  pdf(oncotitiv_filename)
+  plotTiTv(res = Onco.titv)
+  dev.off()
+  #------------------------------------------------------------------------------#
+  current_date <- format(Sys.Date(), "%y%m%d")
+  oncoplot_filename <- paste0(current_date, "_oncoplot.pdf")
+  pdf(oncoplot_filename)
+  oncoplot(maf = Onco,
+           genes = top_genes,
+           colors = Oncoprint_color,
+           leftBarLims = c(0,100),
+           leftBarData = Genes,
+           SampleNamefontSize = as.numeric(get_option("SampleNamefontSize")), 
+           removeNonMutated = as.logical(get_option("removeNonMutated")),
+           writeMatrix = as.logical(get_option("writeMatrix")),
+           showTumorSampleBarcodes = as.logical(get_option("showTumorSampleBarcodes")),
+           legendFontSize = 1,
+           annotationFontSize = 1,
+           anno_height = 2,
+           sampleOrder = sample_order,
+           titleText = plot_title,
+           annotationColor = Annotation_color,
+           clinicalFeatures = clinical_features,
+           top = GeneCut)
+  dev.off()
+  #------------------------------------------------------------------------------#
+  mafsummary_filename <- paste0(current_date, "_mafsummary.pdf")
+  pdf(mafsummary_filename)
+  plotmafSummary(maf = Onco,color = Oncoprint_color, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+  dev.off()
+  #------------------------------------------------------------------------------#
+  maf_lolli <- read.table('EasyOnco.maf', sep='\t', header=T)
+  gene <- subset(maf_lolli, maf_lolli$Variant_Classification!="Intron_variant" & maf_lolli$Variant_Classification !="Silent_Mutation")
+  Intron_gene <- unique(gene$Hugo_Symbol)
+  # idx <- which(maf_lolli$Hugo_Symbol %in% Intron_gene)
+  # maf_lolli <- maf_lolli[-c(idx), ]
+  # lolli_gene <- unique(maf_lolli$Hugo_Symbol)
+  
+  Onco_lolli <-  read.maf(maf = maf_lolli, 
+                          verbose=T,
+                          vc_nonSyn = VariantClassification,
+                          clinicalData = Clinical_annotation)
+  
+  #------------------------------------------------------------------------------#
+  lollipopplot_filename <- paste0(current_date, "_lollipop.pdf")
+  pdf(lollipopplot_filename)
+  for (Gene in Intron_gene) {
+    lollipopPlot(maf = Onco_lolli,
+                 gene = Gene,
+                 AACol = NULL,
+                 showMutationRate = TRUE,
+                 showDomainLabel = FALSE,
+                 labelPos = NULL, 
+                 colors = Oncoprint_color)
+  }
+  dev.off()
+  #------------------------------------------------------------------------------#
+  vafplot_filename <- paste0(current_date, "_vafplot.pdf")
+  pdf(vafplot_filename)
+  plotVaf(maf = Onco, vafCol = 'i_TumorVAF_WU', width = 10, height = 10)
+  dev.off()
+  #------------------------------------------------------------------------------#
+  drugint_filename <- paste0(current_date, "_druginteraction.pdf")
+  pdf(drugint_filename)
+  drugInteractions(maf = Onco, fontSize = 0.75)
+  dev.off()
+  #------------------------------------------------------------------------------#
+} else {
+  #------------------------------------------------------------------------------#
+  maf <- read.table('EasyOnco.maf', sep='\t', header=T)
+  # Clinical_annotation <- read.table('Clinical_annotation.txt', sep='\t', header=T)
+  current_date <- format(Sys.Date(), "%y%m%d")
+  #------------------------------------------------------------------------------#
+  VariantClassification = c("Frame_Shift_Del",
+                            "Frame_Shift_Ins",
+                            "Splice_Site",
+                            "Translation_Start_Site",
+                            "Nonsense_Mutation",
+                            "Nonstop_Mutation",
+                            "In_Frame_Del",
+                            "In_Frame_Ins",
+                            "Missense_Mutation",
+                            "Start_Codon_Del",
+                            "Stop_Codon_Del",
+                            "Duplication",
+                            "Intron_variant")
+  #------------------------------------------------------------------------------#
+  Onco <-  read.maf(maf = maf, 
+                    verbose=T,
+                    vc_nonSyn = VariantClassification)
+  #------------------------------------------------------------------------------#
+  write.mafSummary(maf = Onco, basename = 'Onco')
+  #------------------------------------------------------------------------------#
+  options_df <- read_excel("/labmed/00.Code/EasyOnco/oncoplot_options_v1.xlsx", col_names = TRUE)
+  options_list <- setNames(as.list(options_df$Value), options_df$Options)
+  get_option <- function(option_name) {
+    return(options_list[[option_name]])
+  }
+  #------------------------------------------------------------------------------#
+  Oncoprint_color <- c(
+    Frame_Shift_Del = get_option("Frame_Shift_Del"),
+    Frame_Shift_Ins = get_option("Frame_Shift_Ins"),
+    Splice_Site = get_option("Splice_Site"),
+    Nonsense_Mutation = get_option("Nonsense_Mutation"),
+    In_Frame_Del = get_option("In_Frame_Del"),
+    In_Frame_Ins = get_option("In_Frame_Ins"),
+    Missense_Mutation = get_option("Missense_Mutation"),
+    Multi_Hit = get_option("Multi_Hit"),
+    Translation_Start_Site = get_option("Translation_Start_Site"),
+    Nonstop_Mutation = get_option("Nonstop_Mutation"),
+    Start_Codon_Del = get_option("Start_Codon_Del"),
+    Stop_Codon_Del = get_option("Stop_Codon_Del"),
+    Duplication = get_option("Duplication"),
+    Intron_variant = get_option("Intron_variant"))
+  Oncoprint_color <- Oncoprint_color[!is.na(Oncoprint_color)]
+  #------------------------------------------------------------------------------#
+  top_gene <- if (get_option("Top gene") == "All") NULL else as.numeric(get_option("Top gene"))
+  if (is.null(top_gene)) {
+    top_genes <- getGeneSummary(Onco)$Hugo_Symbol  
+  } else {
+    top_genes <- getGeneSummary(Onco)$Hugo_Symbol[1:top_gene]
+  }
+  GeneCut <- length(top_genes)
+  #------------------------------------------------------------------------------#
+  plot_title <- get_option("Plot title")
+  #------------------------------------------------------------------------------#
+  vaf_plot <- if (get_option("VAF plot") == "yes") "yes" else FALSE
+  if(vaf_plot == "yes") {
+    Genes <- subsetMaf(maf = Onco, 
+                       genes = top_genes,
+                       fields = "i_TumorVAF_WU", 
+                       mafObj = FALSE)[,mean(i_TumorVAF_WU, na.rm = TRUE), Hugo_Symbol]
+    colnames(Genes)[2] = "VAF (%)"
+  } else {
+    Genes <- NULL
+  }
+  #------------------------------------------------------------------------------#
+  if (tolower(options_list$`Top gene`) == 'all') {
+    Oncogenes <- Onco@gene.summary$Hugo_Symbol
+  } else {
+    top_gene_num <- as.numeric(Option$Top_gene)
+    Oncogenes <- Onco@gene.summary$Hugo_Symbol[1:top_gene_num]
+  }
+  GeneCut <- length(Oncogenes)
+  #------------------------------------------------------------------------------#
+  Onco.titv = titv(maf = Onco, plot = FALSE, useSyn = TRUE)
+  oncotitiv_filename <- paste0(current_date, "_onco.titv.pdf")
+  pdf(oncotitiv_filename)
+  plotTiTv(res = Onco.titv)
+  dev.off()
+  #------------------------------------------------------------------------------#
+  current_date <- format(Sys.Date(), "%y%m%d")
+  oncoplot_filename <- paste0(current_date, "_oncoplot.pdf")
+  pdf(oncoplot_filename)
+  oncoplot(maf = Onco, 
+           genes = top_genes,
+           colors = Oncoprint_color,
+           leftBarLims = c(0,100),
+           leftBarData = Genes,
+           SampleNamefontSize = as.numeric(get_option("SampleNamefontSize")), 
+           removeNonMutated = as.logical(get_option("removeNonMutated")),
+           writeMatrix = as.logical(get_option("writeMatrix")),
+           showTumorSampleBarcodes = as.logical(get_option("showTumorSampleBarcodes")),
+           legendFontSize = 1,
+           annotationFontSize = 1,
+           anno_height = 2,
+           titleText = plot_title,
+           annotationColor = Annotation_color,
+           top = GeneCut)
+  dev.off()
+  #------------------------------------------------------------------------------#
+  mafsummary_filename <- paste0(current_date, "_mafsummary.pdf")
+  pdf(mafsummary_filename)
+  plotmafSummary(maf = Onco,color = Oncoprint_color, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+  dev.off()
+  #------------------------------------------------------------------------------#
+  maf_lolli <- read.table('EasyOnco.maf', sep='\t', header=T)
+  gene <- subset(maf_lolli, maf_lolli$Variant_Classification!="Intron_variant" & maf_lolli$Variant_Classification !="Silent_Mutation")
+  Intron_gene <- unique(gene$Hugo_Symbol)
+  # idx <- which(maf_lolli$Hugo_Symbol %in% Intron_gene)
+  # maf_lolli <- maf_lolli[-c(idx), ]
+  # lolli_gene <- unique(maf_lolli$Hugo_Symbol)
+  
+  Onco_lolli <-  read.maf(maf = maf_lolli, 
+                          verbose=T,
+                          vc_nonSyn = VariantClassification)
+  
+  #------------------------------------------------------------------------------#
+  lollipopplot_filename <- paste0(current_date, "_lollipop.pdf")
+  pdf(lollipopplot_filename)
+  for (Gene in Intron_gene) {
+    lollipopPlot(maf = Onco_lolli,
+                 gene = Gene,
+                 AACol = NULL,
+                 showMutationRate = TRUE,
+                 showDomainLabel = FALSE,
+                 labelPos = NULL, 
+                 colors = Oncoprint_color)
+  }
+  dev.off()
+  #------------------------------------------------------------------------------#
+  vafplot_filename <- paste0(current_date, "_vafplot.pdf")
+  pdf(vafplot_filename)
+  plotVaf(maf = Onco, vafCol = 'i_TumorVAF_WU', width = 10, height = 10)
+  dev.off()
+  #------------------------------------------------------------------------------#
+  drugint_filename <- paste0(current_date, "_druginteraction.pdf")
+  pdf(drugint_filename)
+  drugInteractions(maf = Onco, fontSize = 0.75)
+  dev.off()
+  #------------------------------------------------------------------------------# 
+}

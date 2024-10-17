aade82084abc7602541ce2097410d1b9e1ad71ff
LSY <tjdud02972@gmail.com>
2024-10-17 20:43:18 +0900

    241017 update

--- EasyOnco_v1.py
+++ EasyOnco_v1.py
@@ -19,10 +19,7 @@ def mkMAF(file, args, path) :
     global MAF
     for sheet in args.sheets : 
         tmp = pd.read_excel(f'{path}/{file}',sheet_name=sheet, engine='openpyxl')
-        # tmp = tmp[tmp['select'].isin(args.filter)]
-        # pattern = '|'.join([re.sub(r'\*', r'.*', f) for f in args.filter]) 
-        pattern = '|'.join(f for f in args.filter)
-        tmp['select'] = tmp['select'].astype(str)
+        pattern = '|'.join([re.sub(r'\*', r'.*', f) for f in args.filter]) 
         tmp = tmp[tmp['select'].str.contains(pattern, regex=True, na=False)]
         tmp.rename(columns={'gene':'Hugo_Symbol', 'ref':'Reference_Allele', 'alt':'Tumor_Seq_Allele2', 'VAF.var.freq':'i_TumorVAF_WU', 'NM':'i_transcript_name'}, inplace = True)
         tmp['Chromosome'] = tmp['chrom.pos'].apply(lambda x:x.split(':')[0].replace('chr',""))
